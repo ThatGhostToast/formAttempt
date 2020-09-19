d7beeb1daa65a027b313f33c7befcb9e8f24dd3c
Zac Almas <zalmas@my.gcu.edu>
2020-09-19 15:15:59 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace formAttempt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.helloWorldButton = new System.Windows.Forms.Button();
+            this.helloWorldLabel = new System.Windows.Forms.Label();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // helloWorldButton
+            // 
+            this.helloWorldButton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.helloWorldButton.Font = new System.Drawing.Font("Constantia", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helloWorldButton.Location = new System.Drawing.Point(255, 108);
+            this.helloWorldButton.Name = "helloWorldButton";
+            this.helloWorldButton.Size = new System.Drawing.Size(253, 134);
+            this.helloWorldButton.TabIndex = 0;
+            this.helloWorldButton.Text = "Please Click";
+            this.helloWorldButton.UseMnemonic = false;
+            this.helloWorldButton.UseVisualStyleBackColor = false;
+            this.helloWorldButton.Click += new System.EventHandler(this.helloWorldButton_Click);
+            // 
+            // helloWorldLabel
+            // 
+            this.helloWorldLabel.AutoSize = true;
+            this.helloWorldLabel.Font = new System.Drawing.Font("Constantia", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helloWorldLabel.Location = new System.Drawing.Point(265, 245);
+            this.helloWorldLabel.Name = "helloWorldLabel";
+            this.helloWorldLabel.Size = new System.Drawing.Size(0, 40);
+            this.helloWorldLabel.TabIndex = 1;
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Font = new System.Drawing.Font("Javanese Text", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.Location = new System.Drawing.Point(265, 5);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(234, 100);
+            this.welcomeLabel.TabIndex = 2;
+            this.welcomeLabel.Text = "Welcome!";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.helloWorldLabel);
+            this.Controls.Add(this.helloWorldButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button helloWorldButton;
+        private System.Windows.Forms.Label helloWorldLabel;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
